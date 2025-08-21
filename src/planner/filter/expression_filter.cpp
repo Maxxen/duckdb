23a1d038eefd3ca9ecb58ee82690459ae3b8cf2d
Max Gabrielsson <max@gabrielsson.com>
2025-08-21 21:52:46 +0200

    more include

--- src/planner/filter/expression_filter.cpp
+++ src/planner/filter/expression_filter.cpp
@@ -4,6 +4,7 @@
 #include "duckdb/common/types/data_chunk.hpp"
 #include "duckdb/execution/expression_executor.hpp"
 #include "duckdb/planner/expression/bound_function_expression.hpp"
+#include "duckdb/planner/expression/bound_constant_expression.hpp"
 
 namespace duckdb {
 
