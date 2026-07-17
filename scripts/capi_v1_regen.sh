e65fda973933fe11ffae08efdd0dd9b625561443
Evert Lammerts <evertlammerts@users.noreply.github.com>
2026-07-17 21:57:11 +0200

    Add extension header adapter (#137)
    This adds an adapter to generate the extension headers, _and_ removes
    the legacy header generation script and everything related to it. 🥳
    
    The adapter's approach is slightly different from the C adapter's. It
    expects to be given a path to a template for the extension header, then
    validates all existing signatures, then appends all new symbols. This
    allows us to just keep the existing header exactly the way it is, *and*
    validate it, while for the v2 header we can start from scratch.

--- scripts/capi_v1_regen.sh
+++ scripts/capi_v1_regen.sh
@@ -9,4 +9,10 @@ uv run capigen c \
 	--spec-dir api_spec/v1 \
 	-o src/include/duckdb_v1.h
 
+uv run capigen extension_header \
+	--spec-dir api_spec/v1 \
+	--template api_spec/v1/extension/duckdb_extension.h.in \
+	--internal-out src/include/duckdb/main/capi/extension_api.hpp \
+	-o src/include/duckdb_extension.h
+
 uv run --group dev python scripts/format.py HEAD --fix --noconfirm --silent
